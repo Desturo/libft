8958207132f45a6febc5ba53da41e657787c5df0
Nico Schneider <nschneid@1-A-6.42heilbronn.de>
2025-03-17 15:47:26 +0100

    reåmed te file and added te new header

--- ft_memset.c
+++ ft_memset.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   memset.c                                           :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 18:38:23 by nschneid          #+#    #+#             */
-/*   Updated: 2025/03/13 19:00:42 by nschneid         ###   ########.fr       */
+/*   Updated: 2025/03/17 15:39:08 by nschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
-void	*memset(void *b, int c, size_t len)
+void	*ft_memset(void *b, int c, size_t len)
 {
 	size_t i;
 	i = 0;
