1b5cc918d370e42f02257948da38e66398c97c95
Nico Schneider <nschneid@1-A-6.42heilbronn.de>
2025-03-17 15:45:35 +0100

    firs simple implementation of the header

---
+++ libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 15:29:19 by nschneid          #+#    #+#             */
+/*   Updated: 2025/03/17 15:44:22 by nschneid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifdef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+
+int		ft_isalpha(int input);
+int		ft_isdigit(int input);
+int		ft_isalnum(int input);
+int		ft_isascii(int input);
+int		ft_isprint(int input);
+void	*ft_memset(void *b, int c, size_t len);
+
+#endif
+
