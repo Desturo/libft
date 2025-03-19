e70043c2b494490560a88651790526995fb13213
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-19 16:22:08 +0100

    added strlcat to the project

--- libft.h
+++ libft.h
@@ -12,7 +12,7 @@
 
 #ifndef LIBFT_H
 # define LIBFT_H
- 
+
 # include <stdlib.h>
 # include <stdio.h>
 
@@ -26,6 +26,7 @@ void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 
 #endif
 
