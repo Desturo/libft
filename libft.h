70eec0a0fba106eed6f05ac85770ad3b91e4883c
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-19 16:44:42 +0100

    added toupper to project

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 15:29:19 by nschneid          #+#    #+#             */
-/*   Updated: 2025/03/19 13:26:52 by nschneid         ###   ########.fr       */
+/*   Updated: 2025/03/19 16:43:53 by nschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+int		ft_toupper(int c);
 
 #endif
 
