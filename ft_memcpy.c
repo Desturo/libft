bb967245843b540464b5c88aaf0fb01744f07cd3
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-19 19:08:19 +0100

    added protection, if both args are NULL

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 11:31:31 by nschneid          #+#    #+#             */
-/*   Updated: 2025/03/19 12:39:20 by nschneid         ###   ########.fr       */
+/*   Updated: 2025/03/19 19:00:16 by nschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t i;
 	i = 0;
-
+	
+	if (dst == NULL && src == NULL)
+		return (NULL);
 	while (i < n)
 	{
 		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
@@ -27,8 +29,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 
 //int main()
 //{
-//	char src[10] = "I love C!";
-//	char dst[10];
+//	char *src = NULL;
+//	char *dst = NULL;
 //	ft_memcpy(dst, src, 10);
 //	printf("%s\n", dst);
 //	return (0);
