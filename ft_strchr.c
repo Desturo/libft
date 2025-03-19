f1d27206a680acb30da9c1e2b3ed90b00b545273
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-19 17:14:57 +0100

    added functionality for strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 17:02:20 by nschneid          #+#    #+#             */
-/*   Updated: 2025/03/19 17:03:04 by nschneid         ###   ########.fr       */
+/*   Updated: 2025/03/19 17:14:34 by nschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	
+	int	i;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return ((char *)(s + i));
+		i++;
+	}
+	if (s[i] == c)
+			return ((char *)(s + i));
+	return (NULL);
 }
+
+//int	main()
+//{
+//	char str[] = "1234567890";
+//	char c = 'g';
+//	printf("%s\n", ft_strchr(str, c));
+//	return (0);
+//}
