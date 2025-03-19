36e6f193054fd65c771760315d876a0f5f847c45
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-19 13:29:23 +0100

    added ft_strlcpy

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 15:29:19 by nschneid          #+#    #+#             */
-/*   Updated: 2025/03/17 15:44:22 by nschneid         ###   ########.fr       */
+/*   Updated: 2025/03/19 13:26:52 by nschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,11 @@ int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
-int		ft_isprint(int input);
+int		ft_isprint(int c);
 void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
 #endif
 
