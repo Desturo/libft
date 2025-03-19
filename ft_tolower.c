48dbd35140d56b9d4f6e35cef4e90ad4f9b6fc65
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-19 16:59:10 +0100

    implemented tolower functionality

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,10 +6,23 @@
 /*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 16:56:21 by nschneid          #+#    #+#             */
-/*   Updated: 2025/03/19 16:56:41 by nschneid         ###   ########.fr       */
+/*   Updated: 2025/03/19 16:58:53 by nschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	ft_tolower(int c)
+{
+	if (c  >= 'A' && c <= 'Z')
+	{
+		return (c + ('a' - 'A'));
+	}
+	return (c);
+}
 
+int	main()
+{
+	printf("%c\n", ft_tolower('G'));
+	return (0);
+}
