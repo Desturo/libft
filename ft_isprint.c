b521a804dd97874dada70cb85a7f9918af0dffb5
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-19 19:04:10 +0100

    Changed to AND so the proper condition is checked

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,7 +6,7 @@
 /*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 16:49:28 by nschneid          #+#    #+#             */
-/*   Updated: 2025/03/13 16:49:32 by nschneid         ###   ########.fr       */
+/*   Updated: 2025/03/19 18:28:52 by nschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_isprint(int c)
 {
-	return(c >= ' ' || c <= '~');
+	return(c >= ' ' && c <= '~');
 }
 
 //int main()
