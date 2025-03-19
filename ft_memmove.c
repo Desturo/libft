46ef58c0697f1e955b5a9abe52fd3addbe31d7e0
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-19 13:23:05 +0100

    moved i-- up so we cont from len-1 to 0 not len to 1

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 12:43:16 by nschneid          #+#    #+#             */
-/*   Updated: 2025/03/19 13:11:25 by nschneid         ###   ########.fr       */
+/*   Updated: 2025/03/19 13:22:49 by nschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	while (len > 0)
 	{
-		((unsigned char *)dst)[len] = ((unsigned char *)src)[len];
 		len--;
+		((unsigned char *)dst)[len] = ((unsigned char *)src)[len];
 	}
 	return (dst);
 }
 
-int	main()
-{
-	char str[11] = "0123456789";
-	printf("%s\n", (char *)ft_memmove(str+3, str, 5));
-	return (0);
-}
+//int	main()
+//{
+//	char str[11] = "0123456789";
+//	printf("%s\n", (char *)ft_memmove(str+3, str, 5));
+//	return (0);
+//}
