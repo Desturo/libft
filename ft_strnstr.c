497b8cc09c2889f22f8b796e6b94c76a4c9fdfa2
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-21 09:55:36 +0100

    gave strnstr the correct parameters

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,13 +6,13 @@
 /*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 09:50:26 by nschneid          #+#    #+#             */
-/*   Updated: 2025/03/21 09:51:10 by nschneid         ###   ########.fr       */
+/*   Updated: 2025/03/21 09:54:47 by nschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char needle, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	
 }
