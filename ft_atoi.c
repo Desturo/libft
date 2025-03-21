69eeb31c7618243a56cd0b81a503f766cb9628dc
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-21 10:53:38 +0100

    added atoi implementation

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,34 @@
 /*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 10:43:14 by nschneid          #+#    #+#             */
-/*   Updated: 2025/03/21 10:44:08 by nschneid         ###   ########.fr       */
+/*   Updated: 2025/03/21 10:52:50 by nschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#incude "libft.h"
+#include "libft.h"
 
 int	ft_atoi(const char *str)
 {
-	
+	int	i;
+	int	sign;
+	int	out;
+
+	i = 0;
+	sign = 1;
+	out = 0;
+
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+		{
+			if (str[i] == '-')
+				sign = sign * -1;
+			i++;
+		}
+		while (str[i] >= '0' && str[i] <= '9')
+		{
+			out = (out * 10) + (str[i] - '0');
+			i++;
+		}
+		return (out * sign);
 }
