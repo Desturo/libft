6c83542b3a6be5855ab563d609b9cd39de9f199b
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-21 15:19:45 +0100

    added calloc functionality

---
+++ ft_calloc.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/21 14:21:33 by nschneid          #+#    #+#             */
+/*   Updated: 2025/03/21 14:43:50 by nschneid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	unsigned char	*arr;
+	size_t			i;
+
+	arr = malloc(count * size);
+	if (!arr)
+		return (NULL);
+	i = 0;
+	while (i < count * size)
+	{
+		arr[i] = 0;
+		i++;
+	}
+	return (arr);
+}
