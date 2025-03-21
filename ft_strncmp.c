e217fedb080df257728a79070bbd47fdf2e1c11c
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-21 09:06:05 +0100

    added check for when we check up to the null bt don't want to check the null

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -21,18 +21,20 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (s1[i] && s2[i] && i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-	if ((s1[i] == 0 || s2[i] == 0) && s1[i] != s2[i])
-		return (s1[i] - s2[i]);
+	if ((s1[i] == 0 || s2[i] == 0) && (s1[i] != s2[i]) && (i < n))
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 	return (0);
 }
 
+//#include <string.h>
 //int	main()
 //{
-//	char	s1[] = "a1b2c3";
-//	char	s2[] = "a1b2c3d4";
-//	printf("%d\n", ft_strncmp(s1, s2, 11));
+//	char	s1[] = "test1234";
+//	char	s2[] = "test";
+//	printf("%d\n", ft_strncmp(s2, s1, 4));
+//	printf("%d\n", strncmp(s2, s1, 4));
 //	return (0);
 //}
