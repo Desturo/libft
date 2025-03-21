3e945dfe010c89a9c0b50d1dbc6798bb75491809
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-21 10:38:35 +0100

    implemented strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 09:50:26 by nschneid          #+#    #+#             */
-/*   Updated: 2025/03/21 09:54:47 by nschneid         ###   ########.fr       */
+/*   Updated: 2025/03/21 10:37:06 by nschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,36 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	
+	size_t i;
+	size_t j;
+
+	i = 0;
+
+	if (needle[0] == 0)
+		return ((char *)haystack);
+
+	while ((i < len) && haystack[i])
+	{
+		j = 0;
+		while ((haystack[i + j] == needle[j]) && (i + j < len))
+		{
+			if (needle[j + 1] == '\0')
+			{
+				return ((char *)haystack + i);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (NULL);
 }
+
+//int	main()
+//{
+//	char haystack[] = "abcdefghij";
+//	char needle[] = "abc";
+//
+//	printf("%s\n", ft_strnstr(haystack, needle, 2));
+//	return (0);
+//}
