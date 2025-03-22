15d2167b551c826e04ac4e8626fe31cb3419f13d
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-22 16:18:14 +0100

    added null checks

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 15:01:49 by nschneid          #+#    #+#             */
-/*   Updated: 2025/03/22 15:39:07 by nschneid         ###   ########.fr       */
+/*   Updated: 2025/03/22 16:17:25 by nschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*out;
 	size_t	i;
 
+	if (!s1 || !s2)
+		return (NULL);
 	out = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
 	i = 0;
 	if (!out)
@@ -33,8 +35,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 //	char	*s1;
 //	char	*s2;
 //
-//	s1 = "I love ";
-//	s2 = "42!";
+//	s1 = NULL;
+//	s2 = NULL;
 //	printf("%s\n", ft_strjoin(s1, s2));
 //	return (0);
 //}
