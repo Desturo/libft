62561b64d06e49a8e635e5c0370cdfea119e069c
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-22 20:45:22 +0100

    changes tha don't work

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 18:06:11 by nschneid          #+#    #+#             */
-/*   Updated: 2025/03/22 19:07:49 by nschneid         ###   ########.fr       */
+/*   Updated: 2025/03/22 20:44:56 by nschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,76 +22,86 @@ int	wordlen(const char *s, char c)
 	return (out);
 }
 
-int	putwords(const char **out, const char *s, char c)
+void	putwords(char **out, const char *s, char c)
 {
-	int len;
-	int	i;
-	char *in;
+	int		i;
+	int		j;
+	char	*in;
 
-	len = 0;
+	i = 0;
+	while (*s)
+	{
+		j = 0;
+		while (*s == c && *s)
+			s++;
+		if (!*s)
+			return ;
+		in = (char *)ft_calloc(wordlen(s, c) + 1, sizeof(char));
+		if (!in)
+		{
+			while (i >= 0)
+				free((void *)out[i--]);
+			return (free(in));
+		}
+		while (*s != c && *s)
+			in[j++] = *s++;
+		in[j] = 0;
+		out[i++] = in;
+	}
+	out[i] = NULL;
+}
+
+int	wordcount(const char *s, char c)
+{
+	int	out;
+
+	out = 0;
 	while (*s)
 	{
 		while (*s == c && *s)
 			s++;
-		len = wordlen(s, c);
-		in = (char *)ft_calloc(len + 1, sizeof(char));
+		if (*s)
+			out++;
 		while (*s != c && *s)
-			*in++ = *s++;
-		*in = 0;
-		*out++ = in;
+			s++;
 	}
-	*out = 0;
-	return (1);
+	return (out);
 }
 
-	int wordcount(const char *s, char c)
-	{
-		int out;
+char	**ft_split(const char *s, char c)
+{
+	int		len;
+	char	**out;
 
-		out = 0;
-		while (*s)
-		{
-			while (*s == c && *s)
-				s++;
-			if (*s)
-				out++;
-			while (*s != c && *s)
-				s++;
-		}
-		return (out);
-	}
+	len = wordcount(s, c);
+	if (!len)
+		return (NULL);
+	out = (char **)ft_calloc(len + 1, sizeof(char *));
+	if (!out)
+		return (NULL);
+	out[len] = NULL;
+	putwords(out, s, c);
+	return (out);
+}
 
-	char **ft_split(const char *s, char c)
-	{
-		int len;
-		char **out;
+int	main(void)
+{
+	char	sep;
+	char	*s1;
+	char	*s2;
+	char	*s3;
+	char	*s4;
+	char	**out;
 
-		len = wordcount(s, c);
-		if (!len)
-			return (NULL);
-		//{
-		//	out = (char **)ft_calloc(1, sizeof(char *));
-		//	out[0] = NULL;
-		//	return (out);
-		//}
-		out = (char **)ft_calloc(len + 1, sizeof(char *));
-		if (!out)
-			return (NULL);
-		putwords(out, s, c);
-		return (out);
-	}
-
-	int main(void)
-	{
-		char sep;
-		char *s1;
-		char *s2;
-		char *s3;
-
-		sep = ' ';
-		s1 = "  test1     test2 test3   ";
-		s2 = "";
-		s3 = "test";
-		ft_split(s1, sep);
-		return (0);
-	}
+	sep = 'a';
+	s1 = "  test1     test2 test3   ";
+	s2 = "";
+	s3 = "test";
+	s4 = "test";
+	out = ft_split(s2, sep);
+	if (!out)
+		return (1);
+	while (*out)
+		printf("%s\n", *out++);
+	return (0);
+}
