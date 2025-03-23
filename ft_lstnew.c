61373c5f0bd1f8ab3de00bfc48ac9e59f8fe75a8
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-23 17:09:44 +0100

    implemented lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,13 +6,20 @@
 /*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 16:26:51 by nschneid          #+#    #+#             */
-/*   Updated: 2025/03/23 16:27:36 by nschneid         ###   ########.fr       */
+/*   Updated: 2025/03/23 17:07:18 by nschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstnew(void *content)
+t_list	*ft_lstnew(void *content)
 {
+	t_list	*node;
 
+	node = (t_list *)malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	return (node);
 }
