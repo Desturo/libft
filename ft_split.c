ce9e80d2556424bbf7717d4060ff91a277461214
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-23 11:20:59 +0100

    working split

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,24 @@
 /*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/22 18:06:11 by nschneid          #+#    #+#             */
-/*   Updated: 2025/03/22 20:44:56 by nschneid         ###   ########.fr       */
+/*   Updated: 2025/03/22 20:45:53 by nschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	freewords(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i])
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr[i]);
+}
 
 int	wordlen(const char *s, char c)
 {
@@ -38,17 +51,12 @@ void	putwords(char **out, const char *s, char c)
 			return ;
 		in = (char *)ft_calloc(wordlen(s, c) + 1, sizeof(char));
 		if (!in)
-		{
-			while (i >= 0)
-				free((void *)out[i--]);
-			return (free(in));
-		}
+			return (freewords(out));
 		while (*s != c && *s)
 			in[j++] = *s++;
 		in[j] = 0;
 		out[i++] = in;
 	}
-	out[i] = NULL;
 }
 
 int	wordcount(const char *s, char c)
@@ -56,6 +64,8 @@ int	wordcount(const char *s, char c)
 	int	out;
 
 	out = 0;
+	if (!*s)
+		return (1);
 	while (*s)
 	{
 		while (*s == c && *s)
@@ -74,8 +84,6 @@ char	**ft_split(const char *s, char c)
 	char	**out;
 
 	len = wordcount(s, c);
-	if (!len)
-		return (NULL);
 	out = (char **)ft_calloc(len + 1, sizeof(char *));
 	if (!out)
 		return (NULL);
@@ -84,24 +92,24 @@ char	**ft_split(const char *s, char c)
 	return (out);
 }
 
-int	main(void)
-{
-	char	sep;
-	char	*s1;
-	char	*s2;
-	char	*s3;
-	char	*s4;
-	char	**out;
-
-	sep = 'a';
-	s1 = "  test1     test2 test3   ";
-	s2 = "";
-	s3 = "test";
-	s4 = "test";
-	out = ft_split(s2, sep);
-	if (!out)
-		return (1);
-	while (*out)
-		printf("%s\n", *out++);
-	return (0);
-}
+//int	main(void)
+//{
+//	char	sep;
+//	char	*s1;
+//	char	*s2;
+//	char	*s3;
+//	char	*s4;
+//	char	**out;
+//
+//	sep = ' ';
+//	s1 = "  test1     test2 test3   ";
+//	s2 = "      ";
+//	s3 = "test";
+//	s4 = "test";
+//	out = ft_split(s2, sep);
+//	if (!out)
+//		return (1);
+//	while (*out)
+//		printf("%s\n", *out++);
+//	return (0);
+//}
