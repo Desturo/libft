27c380a18bc0d4aa16f9cf14d09cf1a0bf604fbb
Nico Schneider <nschneid@1-A-8.42heilbronn.de>
2025-03-23 18:11:55 +0100

    added lst_addback

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 17:53:48 by nschneid          #+#    #+#             */
-/*   Updated: 2025/03/23 17:54:24 by nschneid         ###   ########.fr       */
+/*   Updated: 2025/03/23 18:02:42 by nschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,8 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
+	t_list	*last;
+
+	last = ft_lstlast(*lst);
+	last->next = new;
 }
