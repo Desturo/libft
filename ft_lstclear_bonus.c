04f68814e1b848c429245e95911a3a107407c474
Nico Schneider <nschneid@1-A-6.42heilbronn.de>
2025-03-24 12:58:25 +0100

    renamed bonus files

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nschneid <nschneid@student.42heilbronn.de  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/23 19:38:00 by nschneid          #+#    #+#             */
+/*   Updated: 2025/03/23 19:55:11 by nschneid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*temp;
+
+	if (!lst || !(*del))
+		return ;
+	while (*lst)
+	{
+		temp = (*lst)->next;
+		ft_lstdelone(*lst, (*del));
+		*lst = temp;
+	}
+}
